2a1087e84e7788cef06541263891485285791472
PrashantBansal <prashant.bansal@kaplan.com>
2017-10-02 14:58:29 -0400

    Removed unused namespaces

--- HelpLinks/HLPages/EnumerationList.aspx.cs
+++ HelpLinks/HLPages/EnumerationList.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using HelpLinks.Entity;
 using HelpLinks.Utility.EnumsUtility;
